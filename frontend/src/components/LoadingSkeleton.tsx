0bf8a43c2d1c9c436fb260a9f6173b9811f7a5d4
AhmetEmreSengul <ahmetemresengul.34@gmail.com>
2026-03-17 18:52:18 +0300

    Update loading skeleton to be more inline with the actual card

--- frontend/src/components/LoadingSkeleton.tsx
+++ frontend/src/components/LoadingSkeleton.tsx
@@ -1,6 +1,10 @@
 const LoadingSkeleton = () => {
   return (
-    <div className="w-full md:w-105 h-80 animate-pulse bg-neutral-300 rounded-lg" />
+    <div className="w-full md:w-105 h-110 animate-pulse bg-neutral-300 rounded-lg justify-between">
+      <div className="w-full h-30 animate-pulse bg-neutral-300"></div>
+      <div className="w-full h-30 animate-pulse bg-neutral-200"></div>
+      <div className="w-full h-20 animate-pulse bg-neutral-300"></div>
+    </div>
   );
 };
 
